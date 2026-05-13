b3d61a011e8c141e0e02a181f707621c54f37697
pwhoae <pwhoae@connect.ust.hk>
2026-05-13 23:47:35 +0800

    Delete godot/技能状态栏 directory

--- godot/技能状态栏/skill_status.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name SkillStatus
-
-@export var skill_id: String = ""
-@export var skill_name: String = ""
-@export var skill_icon: Texture2D
-@export var cool_down: float = 1.0
