2912f6c840deb8478b581a6f41a7df0a15b2d264
pwhoae <pwhoae@connect.ust.hk>
2026-05-13 23:46:44 +0800

    Add files via upload

---
+++ godot/技能状态栏/20260513_testscene.gd
@@ -0,0 +1,13 @@
+
+extends Control
+@onready var skill_label: Label = $Skill_name
+@onready var key_label: Label =$Skill_key
+
+var skills = ["skill1", "skill2", "skill3"]
+
+func bind_skills():
+	pass
+
+
+func use_skill():
+	pass
